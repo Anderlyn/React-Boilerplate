4263f0b2762378ceb7997361b5b577e5f75e23e8
--replace-all <Andre>
2021-02-24 23:15:12 -0600

    boilerplate

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './app.scss';
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#app'));
